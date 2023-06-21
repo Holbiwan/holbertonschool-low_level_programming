9c59654edc911453ff85e182a3255a146839dcb1
Holbiwan <6188@holbertonstudents.com>
2023-06-21 00:08:27 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Program prints all single digit numbers of base 10 from 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+int i;
+
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+
+return (0);
+
+}
