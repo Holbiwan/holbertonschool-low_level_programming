65c9f540c2b502280b38b97043e6846cf87e2fc8
Holbiwan <6188@holbertonstudents.com>
2023-06-21 00:26:37 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints single digit base 10 from 0 followed by a new line.
+ *
+ * Return : 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
