871b8fbdf806106454f4a16c859f6a988f19fd99
Holbiwan <6188@holbertonstudents.com>
2023-06-20 23:52:09 -0700

    3. alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Program prints alphabet in lowercase and uppercase
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
