b851f1d59e48f676574483dea52fee31a1306527
Holbiwan <6188@holbertonstudents.com>
2023-06-30 04:22:36 -0700

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -5,12 +5,13 @@
  * @dest: destination memory area
  * @src: memory area to copy from
  * @n number of bytes to copy
+ * Return: Always 0 (Sucess)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-for (i = 0; i < n; i+i+)
+for (i = 0; i < n; i++)
 dest[i] = src[i];
 
 return (dest);
