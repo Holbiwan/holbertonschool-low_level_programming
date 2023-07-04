ffb0124c0df91475fd17c6ec6337b72a97412b77
Holbiwan <6188@holbertonstudents.com>
2023-07-04 03:15:19 -0700

    6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if input integer is prime number, if not returns 0
+ * @n: number to evaluate as a prime number
+ *
+ * Return: 1 if n is a prime number, 0 if is not
+ */
+int is_prime_number(int n)
+{
+if (n == 1 || n < 1)
+return (0);
+if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)	return (0);
+return (1);
+}
