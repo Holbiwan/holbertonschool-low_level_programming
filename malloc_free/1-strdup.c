a38813b04f54d2c7a53e1ea84170e1b7fee15a99
Holbiwan <6188@holbertonstudents.com>
2023-07-10 22:47:00 -0700

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to copied string (Sucess), NULL( Error)
+ */
+char *_strdup(char *str)
+{
+char *s;
+int i, len;
+i = 0;
+if (!str)
+return (NULL);
+while (*(str +i))
+++i;
+len = i;
+s = malloc(sizeof(char) * (len + 1));
+if (!s)
+return (NULL);
+for (i = 0; i <= len; ++i)
+*(s + i) = *(str + i);
+
+return (s);
+}
