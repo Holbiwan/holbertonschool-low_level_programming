9c6093e7631e9faea1f6ff59239bac6822e4eeff
Holbiwan <6188@holbertonstudents.com>
2023-08-02 09:36:22 -0700

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,8 +20,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	new->prev = NULL;
 	if (*head)
-		(*head)->prev = new_node;
-	*head = new_node;
+		(*head)->prev = new;
+	*head = new;
 
-	return (new_node);
+	return (new);
 }
